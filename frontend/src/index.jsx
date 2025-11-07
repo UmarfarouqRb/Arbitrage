2d6169902fca3984085561b38208b222f3999764
umarfarouqrb <umarfarouqrb@gmail.com>
2025-11-07 10:37:16 +0000

    Update Privy logo

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
       appearance: {
         theme: 'light',
         accentColor: '#676FFF',
-        logo: 'https://your-logo-url',
+        logo: '/logo.png',
       },
       // Create embedded wallets for users who don't have a wallet
       embeddedWallets: {
