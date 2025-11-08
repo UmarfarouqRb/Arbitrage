96e046095e9786b3c80f4585a150b4e60cacf9b0
umarfarouqrb <umarfarouqrb@gmail.com>
2025-11-08 10:42:27 +0000

    fix: Configure Netlify redirects and add Vite polyfills

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env': env
-    },
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-    },
-  }
-})
+export default defineConfig({
+  plugins: [
+    react(),
+    nodePolyfills(),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
