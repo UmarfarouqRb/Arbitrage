f27e7cf6790389992e84483ad0319c7aeeea44e9
umarfarouqrb <umarfarouqrb@gmail.com>
2025-11-10 11:01:03 +0000

    feat: add crypto utility for wallet decryption

---
+++ frontend/functions/utils/crypto.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto');
+
+const ALGORITHM = 'aes-256-ctr';
+const IV_LENGTH = 16;
+
+function decrypt(encryptedData, password) {
+    const textParts = encryptedData.split(':');
+    const iv = Buffer.from(textParts.shift(), 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const key = crypto.createHash('sha256').update(String(password)).digest('base64').substr(0, 32);
+    const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+module.exports = { decrypt };
